de5b6ad80f6af065c3fb2a2cd138cb442eeb7356
riyadekate <43324234+riyadekate@users.noreply.github.com>
2018-10-05 10:14:29 -0500

    u

---
+++ Team Tech Project/Incuser.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 23 17:34:11 2018
+
+@author: c501
+"""
+
+input1 = input("Please input a number")
+if input1 == 6:
+    print('hi')
+    
+    
+    
